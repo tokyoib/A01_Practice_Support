41716c4ec7fcecceec11db9ad5133973e70dbed0
tokyoib <tokyoib.system@gmail.com>
2025-01-24 15:28:30 +0900

    commt

---
+++ practicesupport/src/main/java/com/example/practicesupport/PracticesupportApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicesupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticesupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticesupportApplication.class, args);
+	}
+
+}
---
+++ practicesupport/src/test/java/com/example/practicesupport/PracticesupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicesupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticesupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
